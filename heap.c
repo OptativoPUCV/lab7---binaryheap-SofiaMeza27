97cfcbc62b59807f424a9e09eda897e4186dc562
Your Name <you@example.com>
2023-10-06 18:48:51 -0300

    new try

--- heap.c
+++ heap.c
@@ -37,6 +37,8 @@ void heap_push(Heap* pq, void* data, int priority)
    }
 
    pq -> heapArray[pq -> size].data = data;
+   pq -> heapArray[pq -> size].priority = priority;
+   pq -> size++;
 }
 
 
